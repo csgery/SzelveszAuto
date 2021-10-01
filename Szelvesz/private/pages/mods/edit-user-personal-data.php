c086dd89f89b0e2fe84bd3da53ea0ea66b576165
bogosbalint <bogosbalint2001@gmail.com>
2021-10-01 18:18:52 +0200

    edit-user-personal-data.php: bővítve a html oldal megjelenítéséért felelős kóddal

--- Szelvesz/private/pages/mods/edit-user-personal-data.php
+++ Szelvesz/private/pages/mods/edit-user-personal-data.php
@@ -48,3 +48,71 @@ if(isset($_POST['data-submit'])){
 }
 
 ?>
+
+
+<?php if(!isset($_POST['data-submit']) || $errors): ?>
+
+<div class="card mt-md-auto p-5 align-middle align-content-center">
+    <form method="post" enctype="multipart/form-data" class="form">
+        <div class='card-header mb-2'>
+            <h4>Az alábbi adatokat tudja módosítani:</h4>
+        </div>
+
+        <div class='input-group <?=  $errors['lastname'] ? $errorBorderClass : '' ?>  p-3'>
+            <label for='lastname' class='me-3'>Vezetéknév:</label>
+            <input type="text" id="lastname" name="lastname" placeholder='<?= $_SESSION['user']['lastname'] ?>' class='form-control'>
+        </div>
+
+        <div class='input-group <?=  $errors['firstname'] ? $errorBorderClass : '' ?>  p-3'>
+            <label for='firstname' class='me-3'>Keresztnév:</label>
+            <input type="text" id="firstname" name="firstname" placeholder='<?= $_SESSION['user']['firstname'] ?>' class='form-control'>
+        </div>
+
+        <div class='input-group <?=  $errors['phone_number'] ? $errorBorderClass : '' ?>  p-3'>
+            <label for='phone_number' class='me-3'>Telefonszám:</label>
+            <input type="text" id="phone_number" name="phone_number" placeholder='<?= $_SESSION['user']['phone_number'] ?>' class='form-control'>
+        </div>
+
+
+
+        <?php if ($errors): ?>
+            <div class="alert alert-danger">
+                <h5>A jóváhagyás nem sikerült!</h5>
+                <?php foreach ($errors as $item): ?>
+                    <?php foreach ($item as $error): ?>
+                        - <?= $error ?> <br/>
+                    <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
+
+        <?php endif; ?>
+        <div class='input-group'>
+            <input name ="data-submit" id="data-submit" type="submit" value="Adatok jóváhagyása" class='btn btn-primary' >
+        </div>
+    </form>
+</div>
+</div>
+
+<?php elseif(isset($_POST['data-submit']) && $modificationCounter == 0): ?>
+
+    <table class="table table-dark table-hover table-striped table-borderless table">
+
+        <thead>
+        <tr>
+            <th scope="col" class="text-center m-auto">Ön nem módosította az adatait! <a class="btn-primary ms-3 btn-sm text-decoration-none" href="?p=user-data">Ok</a></th>
+        </tr>
+        </thead>
+    </table>
+
+<?php else: ?>
+    <?php $user = db_fetch('users', 'username LIKE :username', [':username' => $_SESSION['user']['username']]); ?>
+    <?php $_SESSION['user'] = $user; ?>
+    <table class="table table-dark table-hover table-striped table-borderless table">
+
+        <thead>
+        <tr>
+            <th scope="col" class="align-middle text-center m-auto">Sikeresen módosította az adatait! <a class="btn-primary ms-3 btn-sm text-decoration-none" href="?p=user-data">Ok</a></th>
+        </tr>
+        </thead>
+    </table>
+<?php endif; ?>
