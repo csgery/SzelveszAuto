d97f3219901b6b3269ce13b32dab5011c6a00a7b
bogosbalint <bogosbalint2001@gmail.com>
2021-10-01 17:15:26 +0200

    edit-login-data.php: bővítve a html oldal megjelenítéséért felelős kóddal és a már meglévő ellenőrző feltétel kiegészítésével

--- Szelvesz/private/pages/mods/edit-login-data.php
+++ Szelvesz/private/pages/mods/edit-login-data.php
@@ -34,7 +34,7 @@ if(isset($_POST['data-submit'])){
     }
 
     
-    if(empty($errors)) {
+    if(empty($errors && isset($_POST['data-submit']))) {
         db_execute('UPDATE users SET username = :username, email = :email, password = :password WHERE username = :IDusername;', [
 
             ':username' => $_POST['username'],
@@ -48,3 +48,73 @@ if(isset($_POST['data-submit'])){
     }
 }
 ?>
+
+<?php if(!isset($_POST['data-submit']) || $errors): ?>
+
+<div class="card mt-md-auto p-5 align-middle align-content-center">
+    <form method="post" enctype="multipart/form-data" class="form">
+        <div class='card-header mb-2'>
+            <h4>Az alábbi adatokat tudja módosítani:</h4>
+        </div>
+
+        <div class='input-group <?=  $errors['username'] ? $errorBorderClass : '' ?>  p-3'>
+            <label for='username' class='me-3'>Felhasználónév:</label>
+            <input type="text" id="username" name="username" placeholder='<?= $_SESSION['user']['username'] ?>' class='form-control'>
+        </div>
+
+
+        <div class='input-group <?=  $errors['email'] ? $errorBorderClass : '' ?>  p-3'>
+            <label for='email' class='me-3'>Email:</label>
+            <input type="email" id="email" name="email" placeholder='<?= $_SESSION['user']['email'] ?>' class='form-control'>
+        </div>
+
+        <div class='input-group <?=  $errors['password'] ? $errorBorderClass : '' ?>  p-3'>
+            <label for='password' class='me-3'>Jelszó:</label>
+            <input type="password" id="password" name="password" placeholder='********' class='form-control'>
+        </div>
+
+        <div class='input-group <?=  $errors['password'] ? $errorBorderClass : '' ?>  p-3'>
+            <label for='password_confirmation' class='me-3'>Jelszó újra:</label>
+            <input type="password" id="password_confirmation" name="password_confirmation" placeholder='********' class='form-control'>
+        </div>
+
+        <?php if ($errors): ?>
+            <div class="alert alert-danger">
+                <h5>A jóváhagyás nem sikerült!</h5>
+                <?php foreach ($errors as $item): ?>
+                    <?php foreach ($item as $error): ?>
+                        - <?= $error ?> <br/>
+                    <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
+
+        <?php endif; ?>
+        <div class='input-group'>
+            <input name ="data-submit" id="data-submit" type="submit" value="Adatok jóváhagyása" class='btn btn-primary' >
+        </div>
+    </form>
+</div>
+</div>
+
+<?php elseif(isset($_POST['data-submit']) && $modificationCounter == 0): ?>
+
+<table class="table table-dark table-hover table-striped table-borderless table">
+
+    <thead>
+    <tr>
+        <th scope="col" class="text-center m-auto">Ön nem módosította az adatait! <a class="btn btn-primary ms-3 btn-sm" href="?p=login-data">Ok</a></th>
+    </tr>
+    </thead>
+</table>
+
+<?php else: ?>
+
+<table class="table table-dark table-hover table-striped table-borderless table">
+
+    <thead>
+    <tr>
+        <th scope="col" class="align-middle text-center m-auto">Sikeresen módosította az adatait! <a class="btn btn-primary btn-sm" href="?p=login-data">Ok</a></th>
+    </tr>
+    </thead>
+</table>
+<?php endif; ?>
