b8cf7f6fb9f89cdde40eaa93634c056b78b5ca9e
Zettisch Márk <public@zetmark.hu>
2021-10-01 15:10:31 +0200

    Kijelentkezési felület létrehozása, megírása.

---
+++ Szelvesz/private/pages/auth/logout.php
@@ -0,0 +1,10 @@
+<table class="table table-dark table-hover table-striped table-borderless table">
+
+    <thead>
+    <tr>
+
+        <th scope="col" class="text-center m-auto">Sikeres kijelentkezés!<a class="btn-primary ms-3 btn-sm text-decoration-none" href="?p=home">Ok</a></th>
+
+    </tr>
+    </thead>
+</table>
