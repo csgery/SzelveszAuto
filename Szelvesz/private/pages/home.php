1ab4624e12c163fab8434a640ab30138abc360c5
Patrik920225 <patrik920225@freemail.hu>
2021-10-01 11:05:23 +0200

    home.php A kezdőoldal létrehozása, amely a weboldalra való látogatás során először jelenik meg.

---
+++ Szelvesz/private/pages/home.php
@@ -0,0 +1,21 @@
+<?php
+?>
+
+
+<article class="mt-3">
+    <h1 >Rólunk:</h1>
+    <h4 class="mt-2">A Szélvész Autókereskedés nem csupán egy egyszerű autókereskedés.</h4>
+</article>
+
+
+
+<article>
+    <p>
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a metus scelerisque, pulvinar arcu at, gravida leo. Nullam laoreet erat vitae ipsum maximus fermentum. Aliquam eleifend, nisl eget tempus euismod, nulla augue vehicula sem, in accumsan mauris orci ac mauris. Cras feugiat finibus neque, sed placerat diam aliquet vel. Nunc luctus ac enim ac vestibulum. Duis sed est sed tortor vestibulum auctor. Suspendisse potenti. Nam felis mauris, commodo sed porta in, lobortis egestas nulla. Proin egestas mattis massa sit amet luctus. Donec auctor iaculis metus, et dignissim lectus viverra non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper dictum augue sit amet vehicula.
+    </p>
+
+    <p>
+        Nam ultrices quam ut quam gravida, in scelerisque nibh gravida. Maecenas congue turpis enim, et viverra lectus tincidunt ut. Donec id pellentesque enim, at blandit odio. Quisque dignissim dui eget elit scelerisque finibus. Suspendisse in purus ac nulla ornare fringilla sit amet non eros. Proin at gravida est. In commodo posuere augue et facilisis. Duis gravida sem sit amet augue sodales, a porttitor lacus ornare. Nam ex mi, condimentum at urna ut, facilisis pretium lacus. Quisque gravida ipsum sit amet arcu elementum facilisis. Maecenas eleifend, ligula nec consequat condimentum, arcu erat pharetra leo, venenatis accumsan magna odio a orci. In non semper diam, scelerisque pellentesque urna. Suspendisse efficitur tristique tristique. Vestibulum sed justo viverra, viverra orci mollis, laoreet justo.
+    </p>
+</article>
