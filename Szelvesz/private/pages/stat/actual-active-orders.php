4881cd1b7152166136fde639d1a5c77065ff7a97
Zettisch Márk <public@zetmark.hu>
2021-10-02 17:53:58 +0200

    actual-active-orders.php módosítása: a megrendelt autó kilistázása A megrendelt autó adatainak kilistázása táblázat formában.

--- Szelvesz/private/pages/stat/actual-active-orders.php
+++ Szelvesz/private/pages/stat/actual-active-orders.php
@@ -71,3 +71,41 @@
     </tbody>
 
 </table>
+
+<table class="table table-dark table-hover table-striped table-borderless align-middle mx-auto mt-5 mb-4 text-center ">
+    <thead>
+    <tr>
+        <th colspan="6" class="text-primary">Rendelt autó adatai</th>
+    </tr>
+    <tr>
+        <th></th>
+        <th scope="col">Kép</th>
+        <th scope="col">Név</th>
+        <th scope="col">Modell</th>
+        <th scope="col">Szín</th>
+        <th scope="col">Ár</th>
+    </tr>
+    </thead>
+
+    <tbody>
+
+    <?php $counter = 1; ?>
+    <?php foreach($cars as $car):?>
+        <tr>
+            <th scope="row"><?= $counter?>.</th>
+            <td> <img src="<?= $car['image'] ?>" class="img-fluid rounded mx-auto d-block"  alt=""></td>
+            <td><?= $car['name'] ?></td>
+            <td><?= $car['model'] ?></td>
+            <td><?= $car['colour'] ?></td>
+            <td><?= $car['price'] ?> Ft</td>
+        </tr>
+        <?php $counter++; ?>
+    <?php endforeach;?>
+
+
+
+
+
+    </tbody>
+
+</table>
