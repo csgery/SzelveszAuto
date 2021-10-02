700503ca2d9173afa4e6d66ba9e694c6d8738d17
Zettisch Márk <public@zetmark.hu>
2021-10-02 18:33:08 +0200

    Módosítottam a stat.php fájlt. Rendelési statisztika megvalósítása

--- Szelvesz/private/pages/stat/stat.php
+++ Szelvesz/private/pages/stat/stat.php
@@ -34,3 +34,41 @@
         </tbody>
     <?php endif;?>
 </table>
+
+
+<table class="table table-dark table-hover table-striped table-borderless align-middle mx-auto mt-1 text-center ">
+
+
+    <thead>
+    <tr>
+        <th scope="col" colspan="9" class="text-primary">Rendelési statisztika</th>
+    </tr>
+    <tr>
+        <?php $activeOrderCount = db_fetchall('SELECT count(order_id) FROM orders WHERE is_arrived = 0'); ?>
+        <?php if($activeOrderCount[0]["count(order_id)"] < 1):?>
+            <th scope="col">Jelenleg nincs aktív rendelés!</th>
+        <?php else:?>
+            <th scope="col">Aktív rendelések száma</th>
+            <th scope="col">Ebből megerősítésre vár</th>
+            <th scope="col">Ebből előkészítés alatt</th>
+            <th scope="col">Ebből szállítás alatt</th>
+            <th scope="col">Mindenkori teljesített rendelések száma</th>
+    </tr>
+    </thead>
+
+
+
+
+    <tbody>
+    <tr>
+        <td scope="row"><?= $activeOrderCount[0]["count(order_id)"];?></td>
+        <td><?= db_fetchall('SELECT count(order_id) FROM orders WHERE preparing = 0 AND is_arrived = 0')[0]["count(order_id)"];?></td>
+        <td><?= db_fetchall('SELECT count(order_id) FROM orders WHERE preparing = 1 AND is_shipped = 0 AND is_arrived = 0')[0]["count(order_id)"];?></td>
+        <td><?= db_fetchall('SELECT count(order_id) FROM orders WHERE is_shipped = 1 AND is_arrived = 0')[0]["count(order_id)"];?></td>
+        <td><?= db_fetchall('SELECT count(order_id) FROM orders WHERE is_arrived = 1')[0]["count(order_id)"];?></td>
+    </tr>
+
+
+    </tbody>
+        <?php endif;?>
+</table>
