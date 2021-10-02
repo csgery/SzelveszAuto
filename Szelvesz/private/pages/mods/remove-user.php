984b63f41b50b59c6231c30f92480d348655485a
Csépányi Gergely <csepanyigergely@gmail.com>
2021-10-02 09:49:39 +0200

    Módosítottam a korábban előkészített fájlt
    A remove-user.php fájlban implementáltam a HTML megjelenítést.

--- Szelvesz/private/pages/mods/remove-user.php
+++ Szelvesz/private/pages/mods/remove-user.php
@@ -15,3 +15,21 @@ else{
 
 ?>
 
+
+<?php if($activeOrder):?>
+    <table class="table table-dark table-hover table-striped table-borderless table">
+        <thead>
+        <tr>
+            <th scope="col" class="text-center m-auto">Önnek van aktív rendelése, így nem törölheti a fiókját! <a class="btn btn-primary ms-3 btn-sm" href="?p=my-orders">Értem</a></th>
+        </tr>
+        </thead>
+    </table>
+<?php else: ?>
+    <table class="table table-dark table-hover table-striped table-borderless table">
+        <thead>
+        <tr>
+            <th scope="col" class="text-center m-auto">Biztos, hogy törölni kívánja fiókját? (A törlés nem vonható vissza!) <a class="btn btn-primary ms-3 btn-sm" href="?p=mods/remove-user-finally">Igen</a><a class="btn btn-primary ms-3 btn-sm" href="?p=home">Nem</a></th>
+        </tr>
+        </thead>
+    </table>
+<?php endif; ?>
