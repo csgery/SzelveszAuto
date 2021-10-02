20ad78d3de0e9fd89392cf430e38bd8b1d54bde1
bogosbalint <bogosbalint2001@gmail.com>
2021-10-02 14:46:55 +0200

    Módosítottam a buy.php fájlt. A vásárláshoz szükséges form implementálása

--- Szelvesz/private/pages/buy.php
+++ Szelvesz/private/pages/buy.php
@@ -63,5 +63,60 @@
         </tfoot>
 
     </table>
+
+    <form method="post">
+        <table class="table table-dark table-hover table-striped table-borderless text-center align-middle mx-auto mt-1">
+            <thead>
+                <tr>
+                    <th scope="col" colspan="5" class="text-warning">Rendelni kívánt termék:</th>
+                </tr>
+
+                <tr>
+                    <th scope="col">Kép</th>
+                    <th scope="col">Név</th>
+                    <th scope="col">Modell</th>
+                    <th scope="col">Szín</th>
+                    <th scope="col">Ár</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td scope="row"> <img src="<?= $car['image'] ?>" class="img-fluid rounded mx-auto d-block"  alt=""></td>
+                    <td><?= $car['name'] ?></td>
+                    <td><?= $car['model'] ?></td>
+                    <td><?= $car['colour'] ?></td>
+                    <td><?= $car['price'] ?> Ft</td>
+                </tr>
+            </tbody>
+
+            <tfoot>
+                <th scope="row" class="align-middle" colspan="7">
+                    <div class="d-grid gap-2">
+                        <button type="button" value="" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Adatok jóváhagyása és vásárlás</button>
+                    </div>
+                </th>
+            </tfoot>
+
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Rendelés</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            A rendeléssel kijelentem, hogy az általam megadott adatok a valóságnak megfelelnek.
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Vissza</button>
+                            <input name ="shopping-submit" type="submit" value="Rendelés" class="btn btn-outline-warning" >
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </table>
+    </form>
 <?php endif; ?>
 
