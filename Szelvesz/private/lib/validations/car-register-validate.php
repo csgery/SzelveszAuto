d51db244aae0c7362892dc6a6a86ffb27c97f0d0
Patrik920225 <patrik920225@freemail.hu>
2021-10-02 11:01:20 +0200

    Létrehoztam az autók felvételéhez szükséges ellenőrzéseket. Kapcsolódó fájl: car-register-validate.php

---
+++ Szelvesz/private/lib/validations/car-register-validate.php
@@ -0,0 +1,61 @@
+<?php
+
+function validate_car_reg(array $request): array{
+    $error = [];
+
+    if(empty($request['name'])){
+        $error['name'][] = 'Autó neve nem lehet üres!';
+    }else if(strlen($request['name']) < 2) {
+        $error['name'][] = 'Autó neve legalább 2 karakter!';
+    }
+
+
+    
+
+    if(empty($request['price'])){
+        $error['price'][] = 'Autó ára nem lehet üres!';
+    }elseif($request['price'] < 1){
+        $error['price'][] = 'Autó ára nem lehet kisebb, mint 1!';
+    }elseif($request['price'] > 2_100_000_000){
+        $error['price'][] = 'Autó ára nem lehet nagyobb, mint 2,1 milliárd!';
+    }
+
+
+    $carInDB = db_fetch('cars', 'name LIKE :name AND model LIKE :model AND colour LIKE :colour AND price LIKE :price AND is_deleted = 0', [
+        ':name' => $_POST['name'],
+        ':model' => $_POST['model'],
+        ':colour' => $_POST['colour'],
+        ':price' => $_POST['price']
+    ]);
+
+    if($carInDB){
+        $error['notUnique'][] = 'Ez az autó már fel van töltve!';
+    }
+
+
+    return $error;
+}
+
+function validate_car_edit(array $request): array{
+    $error = [];
+
+    if(!empty($request['name'])){
+
+        if(strlen($request['name']) < 2) {
+            $error['name'][] = 'Autó neve legalább 2 karakter!';
+        }
+    }
+
+    if(!empty($request['price'])) {
+
+        if ($request['price'] < 1) {
+            $error['price'][] = 'Autó ára nem lehet kisebb, mint 1!';
+        } elseif ($request['price'] > 2_100_000_000) {
+            $error['price'][] = 'Autó ára nem lehet nagyobb, mint 2,1 milliárd!';
+        }
+    }
+
+
+
+    return $error;
+}
