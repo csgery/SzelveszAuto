16c7cd2377cacfc00169fcce9560b293e9f4fe29
bogosbalint <bogosbalint2001@gmail.com>
2021-10-02 17:11:55 +0200

    Módosítottam a my-orders.php fájlt. Implementáltam a felhasználók saját passzív rendeléseinek kilistázásához szükséges kódot

--- Szelvesz/private/pages/my-orders.php
+++ Szelvesz/private/pages/my-orders.php
@@ -76,3 +76,57 @@ $myPassiveOrders = db_fetchall('SELECT * FROM orders WHERE username LIKE :userna
         <th scope="row" colspan="9"><a href="?p=cars" class="text-decoration-none">Új autó vásárlása</a></th>
     </tfoot>
 </table>
+
+<?php if(!empty($myPassiveOrders)): ?>
+    <table class="table table-dark table-hover table-striped table-borderless align-middle mx-auto mt-4 text-center ">
+            <thead>
+            <tr>
+                <th scope="col" colspan="9" class="text-primary">Rendelési előzményeim</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th scope="col">Kép</th>
+                <th scope="col">Rendelési azonosító</th>
+                <th scope="col">Rendelés ideje</th>
+                <th scope="col">Név</th>
+                <th scope="col">Modell</th>
+                <th scope="col">Szín</th>
+                <th scope="col">Ár (Ft)</th>
+                <th scope="col">Kézbesítés ideje</th>
+            </tr>
+            </thead>
+        <tbody>
+
+        <?php $counter = 1; ?>
+        <?php foreach($myPassiveOrders as $myOrder): ?>
+            <?php $car = db_fetch('cars', 'id LIKE :car_id', [':car_id' => $myOrder['car_id']]);?>
+            <tr>
+                <th scope="row"><?= $counter?>.</th>
+                <td> <img src="<?= $car['image'] ?>" class="img-fluid rounded mx-auto d-block"  alt=""></td>
+                <td><?= $myOrder['order_id']?></td>
+                <td><?= $myOrder['order_date']?></td>
+                <td><?= $car['name'] ?></td>
+                <td><?= $car['model'] ?></td>
+                <td><?= $car['colour'] ?></td>
+                <td><?= $car['price'] ?></td>
+                <td><?= $myOrder['arrived_at']?></td>
+            </tr>
+            <?php $counter++; ?>
+        <?php endforeach; ?>
+        </tbody>
+
+        <tfoot>
+            <th scope="row" colspan="9"><a href="?p=cars" class="text-decoration-none">Új autó vásárlása</a></th>
+        </tfoot>
+    </table>
+
+<?php else: ?>
+<table class="table table-dark table-hover table-striped table-borderless align-middle mx-auto mt-5 text-center ">
+    <thead>
+    <tr>
+        <th scope="col">Önnek nincs már teljesített rendelése!</th>
+    </tr>
+    </thead>
+</table>
+
+<?php endif; ?>
