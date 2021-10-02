5ce334fdcfabf41ee90abd3a328649a5caaf0f91
Zettisch Márk <public@zetmark.hu>
2021-10-02 18:41:50 +0200

    Módosítottam a stat.php fájlt. Rendelések összegzése táblázat formában.

--- Szelvesz/private/pages/stat/stat.php
+++ Szelvesz/private/pages/stat/stat.php
@@ -3,6 +3,7 @@
 
 ?>
 
+
 <table class="table table-dark table-hover table-striped table-borderless align-middle mx-auto mt-5 text-center ">
     <?php $usersCount = db_fetchall('SELECT count(username) FROM users') ?>
     <thead>
@@ -34,7 +35,6 @@
         </tbody>
     <?php endif;?>
 </table>
-
 
 <table class="table table-dark table-hover table-striped table-borderless align-middle mx-auto mt-1 text-center ">
 
@@ -71,4 +71,75 @@
 
     </tbody>
         <?php endif;?>
+</table>
+
+<table class="table table-dark table-hover table-striped table-borderless align-middle mx-auto mt-1 text-center ">
+
+    <thead>
+    <?php $orderers = db_fetchall('SELECT distinct(username) FROM orders'); ?>
+        <tr>
+            <th scope="col" colspan="9" class="text-primary">Rendelések</th>
+        </tr>
+    <?php if($orderers[0] < 1):?>
+        <tr>
+            <th scope="col" colspan="9" class="text-center justify-content-center">Jelenleg nincsenek rendelések!</th>
+        </tr>
+    <?php else:?>
+        <tr>
+            <th scope="col"></th>
+            <th scope="col">Megrendelő felhasználóneve</th>
+            <th scope="col">Megrendelő email címe</th>
+            <th scope="col">Aktív rendelései</th>
+            <th scope="col">Aktív rendeléseinek ára (Ft)</th>
+            <th scope="col">Passzív rendelései</th>
+            <th scope="col">Passzív rendeléseinek ára (Ft)</th>
+        </tr>
+    <?php endif;?>
+    </thead>
+
+
+    <tbody>
+    <?php $counter = 1; ?>
+    <?php foreach ($orderers as $orderer):?>
+        <?php $activeOrderCount = db_fetch('SELECT count(order_id) FROM orders WHERE username LIKE :username AND is_arrived = 0', ['username' => $orderer['username']]); ?>
+        <?php $activeOrderPrice = db_fetch('SELECT sum(price) FROM cars INNER JOIN orders ON orders.car_id = cars.id WHERE username = :username AND is_arrived =0 ', [':username'=> $orderer['username']]); ?>
+        <?php $passiveOrderCount = db_fetch('SELECT count(order_id) FROM orders WHERE username LIKE :username AND is_arrived = 1', ['username' => $orderer['username']]) ?>
+        <?php $passiveOrderPrice = db_fetch('SELECT sum(price) FROM cars INNER JOIN orders ON orders.car_id = cars.id WHERE username = :username AND is_arrived =1 ', [':username'=> $orderer['username']]); ?>
+       <tr>
+            <th scope="row"><?= $counter ?>.</th>
+            <td><?= $orderer['username'];?></td>
+
+            <td><?= db_fetch('email','users', 'username LIKE :username', [':username' => $orderer['username']])['email'];?></td>
+
+            <?php if($activeOrderCount[0]['count(order_id)'] < 1):?>
+                <td>0</td>
+            <?php else: ?>
+                <td><a href="?p=stat/actual-active-orders&username=<?= $orderer['username'] ?>" class="text-decoration-none link-info"><?= $activeOrderCount [0]["count(order_id)"];?></a></td>
+            <?php endif;?>
+
+            <?php if(strlen($activeOrderPrice[0]['sum(price)']) < 1): ?>
+                <td>0</td>
+            <?php else: ?>
+                <td><?= $activeOrderPrice[0]['sum(price)'];?></td>
+            <?php endif;?>
+
+            <?php if($passiveOrderCount[0]['count(order_id)'] < 1):?>
+                <td>0</td>
+            <?php else: ?>
+                <td><a href="?p=stat/actual-passive-orders&username=<?= $orderer['username'] ?>" class="text-decoration-none link-info"><?= $passiveOrderCount [0]["count(order_id)"];?></a></td>
+            <?php endif;?>
+
+            <?php if(strlen($passiveOrderPrice[0]['sum(price)']) < 1): ?>
+                <td>0</td>
+            <?php else: ?>
+                <td><?= $passiveOrderPrice[0]['sum(price)'];?></td>
+            <?php endif;?>
+        </tr>
+        <?php $counter ++; ?>
+    <?php endforeach;?>
+
+
+    </tbody>
+
+
 </table>
