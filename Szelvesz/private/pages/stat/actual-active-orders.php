9e2daff0deca455061c519882bce67dfe68fe6e2
Zettisch Márk <public@zetmark.hu>
2021-10-02 17:46:48 +0200

    actual-active-orders.php módosítása: személyes adatok kilistázása A lekérdezett személyes adatok kilistázása táblázatba.

--- Szelvesz/private/pages/stat/actual-active-orders.php
+++ Szelvesz/private/pages/stat/actual-active-orders.php
@@ -7,3 +7,34 @@
     $cars = db_fetchall('SELECT * FROM cars INNER JOIN orders ON cars.id = orders.car_id WHERE username = :username AND is_arrived = 0;', [':username'=>$_GET['username']]);
 
 ?>
+
+
+<table class="table table-dark table-hover table-striped table-borderless align-middle mx-auto mt-5 text-center ">
+    <thead>
+    <tr>
+        <th scope="col" colspan="5" class="text-primary">Személyes adatok</th>
+    </tr>
+    <tr>
+        <th scope="col">Felhasználónév</th>
+        <th scope="col">Vezetéknév</th>
+        <th scope="col">Keresztnév</th>
+        <th scope="col">Email</th>
+        <th scope="col">Telefonszám</th>
+
+    </tr>
+    </thead>
+
+    <tbody>
+
+
+    <tr>
+        <td scope="row"> <?= $user['username'] ?> </td>
+        <td><?= $user['lastname'] ?></td>
+        <td><?= $user['firstname'] ?></td>
+        <td><?= $user['email'] ?></td>
+        <td><?= $user['phone_number'] ?></td>
+    </tr>
+
+    </tbody>
+
+</table>
